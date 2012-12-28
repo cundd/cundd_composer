ce80ed58bc8353d8fc5e8789d9f9d0130ffcd554
Daniel Corn <d@cundd.net>
2012-12-28 18:07:24 +0100

    - Removed the TCA records

--- ext_tables.php
+++ ext_tables.php
@@ -27,33 +27,33 @@ if (TYPO3_MODE === 'BE') {
 
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Cundd Composer');
 
-t3lib_extMgm::addLLrefForTCAdescr('tx_cunddcomposer_domain_model_package', 'EXT:cundd_composer/Resources/Private/Language/locallang_csh_tx_cunddcomposer_domain_model_package.xml');
-t3lib_extMgm::allowTableOnStandardPages('tx_cunddcomposer_domain_model_package');
-$TCA['tx_cunddcomposer_domain_model_package'] = array(
-	'ctrl' => array(
-		'title'	=> 'LLL:EXT:cundd_composer/Resources/Private/Language/locallang_db.xml:tx_cunddcomposer_domain_model_package',
-		'label' => 'name',
-		'tstamp' => 'tstamp',
-		'crdate' => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'dividers2tabs' => TRUE,
+// t3lib_extMgm::addLLrefForTCAdescr('tx_cunddcomposer_domain_model_package', 'EXT:cundd_composer/Resources/Private/Language/locallang_csh_tx_cunddcomposer_domain_model_package.xml');
+// t3lib_extMgm::allowTableOnStandardPages('tx_cunddcomposer_domain_model_package');
+// $TCA['tx_cunddcomposer_domain_model_package'] = array(
+// 	'ctrl' => array(
+// 		'title'	=> 'LLL:EXT:cundd_composer/Resources/Private/Language/locallang_db.xml:tx_cunddcomposer_domain_model_package',
+// 		'label' => 'name',
+// 		'tstamp' => 'tstamp',
+// 		'crdate' => 'crdate',
+// 		'cruser_id' => 'cruser_id',
+// 		'dividers2tabs' => TRUE,
 
-		'versioningWS' => 2,
-		'versioning_followPages' => TRUE,
-		'origUid' => 't3_origuid',
-		'languageField' => 'sys_language_uid',
-		'transOrigPointerField' => 'l10n_parent',
-		'transOrigDiffSourceField' => 'l10n_diffsource',
-		'delete' => 'deleted',
-		'enablecolumns' => array(
-			'disabled' => 'hidden',
-			'starttime' => 'starttime',
-			'endtime' => 'endtime',
-		),
-		'searchFields' => 'name,description,authors,version,type,homepage,time,license,tx_cunddcomposer_require,require_dev,',
-		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Package.php',
-		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_cunddcomposer_domain_model_package.gif'
-	),
-);
+// 		'versioningWS' => 2,
+// 		'versioning_followPages' => TRUE,
+// 		'origUid' => 't3_origuid',
+// 		'languageField' => 'sys_language_uid',
+// 		'transOrigPointerField' => 'l10n_parent',
+// 		'transOrigDiffSourceField' => 'l10n_diffsource',
+// 		'delete' => 'deleted',
+// 		'enablecolumns' => array(
+// 			'disabled' => 'hidden',
+// 			'starttime' => 'starttime',
+// 			'endtime' => 'endtime',
+// 		),
+// 		'searchFields' => 'name,description,authors,version,type,homepage,time,license,tx_cunddcomposer_require,require_dev,',
+// 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Package.php',
+// 		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_cunddcomposer_domain_model_package.gif'
+// 	),
+// );
 
 ?>
