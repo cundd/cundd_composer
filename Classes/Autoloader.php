c41dfdebb326e8f5488e2348ee2578949ec6e7bd
Daniel Corn <info@cundd.net>
2016-10-10 17:38:41 +0200

    Load ComposerGeneralUtility if it wasn't loaded

--- Classes/Autoloader.php
+++ Classes/Autoloader.php
@@ -40,6 +40,9 @@ class Autoloader {
 	 * @return void
 	 */
 	static public function register() {
+	    if (!class_exists(ComposerGeneralUtility::class)) {
+            require_once __DIR__ . '/Utility/GeneralUtility.php';
+        }
 		if (file_exists(ComposerGeneralUtility::getPathToVendorDirectory() .  'autoload.php')) {
 			include_once(ComposerGeneralUtility::getPathToVendorDirectory() . 'autoload.php');
 		}
